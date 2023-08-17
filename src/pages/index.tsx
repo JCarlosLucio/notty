e93f716fed8abffd6fb5540470a9e009d98b3edf
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-08-17 12:36:42 -0600

    Change h1 color/text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,9 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            <span className="bg-gradient-to-r from-emerald-500 to-teal-200 bg-clip-text text-transparent">
+              Notty
+            </span>
           </h1>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
