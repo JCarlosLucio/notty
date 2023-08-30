f9702b29ad2cb195d4136ddded75cf27fc7cb246
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-08-30 16:56:00 -0600

    Add ThemeToggle to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import ThemeToggle from "./ThemeToggle";
+
 const Nav = () => {
   return (
     <header className="fixed z-20 h-16 w-full px-5 backdrop-blur-md md:px-32 2xl:px-60">
@@ -10,6 +12,11 @@ const Nav = () => {
         >
           Notty
         </Link>
+        <div className="flex items-center">
+          <div className="ml-5 flex h-7 items-center border-l border-gray-400 pl-5 dark:border-gray-500">
+            <ThemeToggle />
+          </div>
+        </div>
       </nav>
     </header>
   );
