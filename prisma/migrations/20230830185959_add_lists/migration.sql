9cf7dd38fdcd0dc1e21c55cf6a62484f0de5141a
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-08-30 13:03:44 -0600

    Add List model - Run migration to add List

---
+++ prisma/migrations/20230830185959_add_lists/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "List" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "List_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
