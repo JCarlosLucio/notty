7dab87652c4fd038d10a6421ff1cf44a6d5831e8
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-08-31 12:06:33 -0600

    Add padding top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="description" content="A note taking app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <main className="flex min-h-screen flex-col items-center justify-center pt-16">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             <span className="bg-gradient-to-r from-emerald-500 to-teal-300 bg-clip-text text-transparent">
