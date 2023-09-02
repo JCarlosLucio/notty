d30a52e5987986110bdf926eb5f23fa4378b3e83
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-01 18:44:41 -0600

    Add ListForm component

---
+++ src/components/ListForm.tsx
@@ -0,0 +1,43 @@
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const ListForm = () => {
+  const form = useForm();
+
+  const onSubmit = () => {
+    console.log("submitting");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Your list title..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ListForm;
