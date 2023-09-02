1763e47ef29e4803b058ae49bbf66319dc606cf1
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-02 12:04:57 -0600

    Add max length for title in createListSchema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
 export const createListSchema = z.object({
-  title: z.string().trim().min(1, "Title is required"),
+  title: z
+    .string()
+    .trim()
+    .min(1, "Title is required")
+    .max(256, "Title must contain at most 256 characters"),
 });
