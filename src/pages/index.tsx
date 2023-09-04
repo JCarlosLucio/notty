b066795414a418f30a791bd4df0230f445ae5716
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-04 00:45:04 -0600

    Fix text color of Logged in message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ function AuthShowcase() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
+      <p className="text-center text-2xl text-secondary-foreground">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
