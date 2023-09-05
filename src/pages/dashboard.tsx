bfc5fb2348ff3fd4f1fa9ddbe5c84ed9c3eb59f0
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-05 00:42:57 -0600

    Add delete button for lists

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,9 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
 import { type GetServerSideProps } from "next";
 
 import CreateList from "@/components/CreateList";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/utils/api";
 
@@ -21,14 +24,45 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const Dashboard = () => {
   const { data: lists } = api.list.getAll.useQuery();
 
+  const { toast } = useToast();
+  const ctx = api.useContext();
+
+  const { mutate: deleteList } = api.list.delete.useMutation({
+    onSuccess: (deletedListId) => {
+      ctx.list.getAll.setData(undefined, (oldList) => {
+        return oldList && deletedListId
+          ? oldList.filter((list) => list.id !== deletedListId)
+          : oldList;
+      });
+      toast({
+        description: "Your list was deleted.",
+      });
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        description: "Something went wrong.",
+      });
+    },
+  });
+
   return (
     <main className="min-h-screen pt-16">
       <CreateList />
 
       <div>
-        <p>The lists</p>
+        <p>The lists {lists?.length}</p>
         {lists?.map((list) => (
-          <p key={list.id}>{list.title}</p>
+          <p key={list.id}>
+            {list.title}
+            <Button
+              variant="destructive"
+              size="icon"
+              onClick={() => deleteList({ id: list.id })}
+            >
+              <Cross2Icon />
+            </Button>
+          </p>
         ))}
       </div>
     </main>
