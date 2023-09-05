8d02b2dd4830aa6f887b9c395519bd403193b2af
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-05 00:08:48 -0600

    Move Avatar/sign in/out Button into Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-import ThemeToggle from "./ThemeToggle";
+import ThemeToggle from "@/components/ThemeToggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 const Nav = () => {
+  const { data: sessionData } = useSession();
+
   return (
     <header className="fixed z-20 h-16 w-full border-b px-5 backdrop-blur-md md:px-32 2xl:px-60">
       <nav className="flex h-full items-center justify-between">
@@ -15,6 +20,27 @@ const Nav = () => {
         <div className="flex items-center">
           <div className="mr-5 flex h-7 items-center border-r pr-5">
             <ThemeToggle />
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            {sessionData && (
+              <>
+                <Avatar>
+                  <AvatarImage src={sessionData?.user.image ?? undefined} />
+                  <AvatarFallback>
+                    {sessionData?.user.name?.at(0) ?? "N"}
+                  </AvatarFallback>
+                </Avatar>
+
+                <p>{sessionData?.user?.name}</p>
+              </>
+            )}
+            <Button
+              size="lg"
+              variant="secondary"
+              onClick={sessionData ? () => void signOut() : () => void signIn()}
+            >
+              {sessionData ? "Sign out" : "Sign in"}
+            </Button>
           </div>
         </div>
       </nav>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,6 @@
 import { type GetServerSideProps } from "next";
-import { signOut, useSession } from "next-auth/react";
 
 import CreateList from "@/components/CreateList";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/utils/api";
 
@@ -22,25 +19,10 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 };
 
 const Dashboard = () => {
-  const { data: sessionData } = useSession();
-
   const { data: lists } = api.list.getAll.useQuery();
 
   return (
     <main className="min-h-screen pt-16">
-      <div className="flex flex-row items-center gap-2">
-        <Avatar>
-          <AvatarImage src={sessionData?.user.image ?? undefined} />
-          <AvatarFallback>
-            {sessionData?.user.name?.at(0) ?? "N"}
-          </AvatarFallback>
-        </Avatar>
-
-        <p>{sessionData?.user?.name}</p>
-      </div>
-
-      <Button onClick={() => void signOut()}>Sign Out</Button>
-
       <CreateList />
 
       <div>
