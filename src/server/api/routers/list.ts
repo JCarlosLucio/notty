a6c33b1ca33fd5c76c764243face559879ca02cc
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-07 01:14:49 -0600

    Add getById procedure

--- src/server/api/routers/list.ts
+++ src/server/api/routers/list.ts
@@ -1,9 +1,31 @@
 import { TRPCError } from "@trpc/server";
 
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { createListSchema, deleteListSchema } from "@/utils/schemas";
+import {
+  createListSchema,
+  deleteListSchema,
+  getByIdListSchema,
+} from "@/utils/schemas";
 
 export const listRouter = createTRPCRouter({
+  getById: protectedProcedure
+    .input(getByIdListSchema)
+    .query(async ({ ctx, input }) => {
+      const list = await ctx.prisma.list.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      const listBelongsToUser = list?.userId === ctx.session.user.id;
+
+      if (!listBelongsToUser) {
+        throw new TRPCError({ code: "FORBIDDEN" });
+      }
+
+      return list;
+    }),
+
   getAll: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.list.findMany({
       where: {
