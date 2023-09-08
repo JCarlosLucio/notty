ccd27a252d405aecd2020deb54a44a4f6422a794
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-08 00:20:08 -0600

    Add dashboard index page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Lists from "@/components/Lists";
+
+const Dashboard = () => {
+  return (
+    <main className="min-h-screen pt-16">
+      <h1>Your dashboard</h1>
+      <Lists />
+    </main>
+  );
+};
+
+export default Dashboard;
