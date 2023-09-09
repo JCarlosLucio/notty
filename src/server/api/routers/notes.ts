392b835a9b9650ffe409343e109577602ec50570
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-09 11:45:30 -0600

    Add create procedure to noteRouter

--- src/server/api/routers/notes.ts
+++ src/server/api/routers/notes.ts
@@ -1,3 +1,15 @@
-import { createTRPCRouter } from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { createNoteSchema } from "@/utils/schemas";
 
-export const noteRouter = createTRPCRouter({});
+export const noteRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(createNoteSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.note.create({
+        data: {
+          listId: input.listId,
+          content: input.content,
+        },
+      });
+    }),
+});
