50ac5806b96b1184f7c95b528a432820db796b2e
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-09 11:23:42 -0600

    Add getting session on getServerSideProps - Redirect if not authenticated

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,21 @@
+import { type GetServerSideProps } from "next";
+
 import Lists from "@/components/Lists";
+import { getServerAuthSession } from "@/server/auth";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: { session } };
+};
 
 const Dashboard = () => {
   return (
