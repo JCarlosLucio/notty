0905fe9a5425831773125e9af0e905e03ef55801
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-11 00:20:22 -0600

    Add getAll procedure to note router

--- src/server/api/routers/notes.ts
+++ src/server/api/routers/notes.ts
@@ -1,7 +1,28 @@
+import { TRPCError } from "@trpc/server";
+
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { createNoteSchema } from "@/utils/schemas";
+import { createNoteSchema, getAllNoteSchema } from "@/utils/schemas";
 
 export const noteRouter = createTRPCRouter({
+  getAll: protectedProcedure
+    .input(getAllNoteSchema)
+    .query(async ({ ctx, input }) => {
+      const notes = await ctx.prisma.note.findMany({
+        where: {
+          listId: input.listId,
+        },
+        orderBy: {
+          updatedAt: "desc",
+        },
+      });
+
+      if (!notes) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return notes;
+    }),
+
   create: protectedProcedure
     .input(createNoteSchema)
     .mutation(async ({ ctx, input }) => {
