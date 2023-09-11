6c72254d806924a8674ddc88449f96e9f3f0ab52
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-11 00:21:26 -0600

    Add notes to ListPage - Get all notes from listId - Show notes

--- src/pages/dashboard/[listId].tsx
+++ src/pages/dashboard/[listId].tsx
@@ -23,16 +23,22 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const ListPage = () => {
   const router = useRouter();
   const listId = router.query.listId ?? "";
+  const id = Array.isArray(listId) ? (listId[0] ? listId[0] : "") : listId;
 
-  const { data: currentList } = api.list.getById.useQuery({
-    id: Array.isArray(listId) ? (listId[0] ? listId[0] : "") : listId,
-  });
+  const { data: currentList } = api.list.getById.useQuery({ id });
+  const { data: notes } = api.note.getAll.useQuery({ listId: id });
 
   return (
     <main className="min-h-screen pt-16">
       <Lists currentListId={currentList?.id} />
+
       <h1>{currentList?.title}</h1>
+
       {currentList && <CreateNote listId={currentList.id} />}
+
+      {notes?.map((note) => {
+        return <p key={note.id}>{note.content}</p>;
+      })}
     </main>
   );
 };
