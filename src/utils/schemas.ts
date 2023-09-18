4a7c5db6653e016f956cff368586bfd72e4f6452
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-18 00:08:02 -0600

    Add createBoardSchema

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -5,6 +5,14 @@ export const getByIdBoardSchema = z.object({
   id: z.string(),
 });
 
+export const createBoardSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, "Title is required")
+    .max(256, "Title must contain at most 256 characters"),
+});
+
 // LIST
 export const getByIdListSchema = z.object({
   id: z.string(),
