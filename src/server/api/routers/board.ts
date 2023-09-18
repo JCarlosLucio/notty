e22153998efd5ae39ba6266df8b86c6eeb4e4825
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-17 19:07:40 -0600

    Add getById procedure to boardRouter

--- src/server/api/routers/board.ts
+++ src/server/api/routers/board.ts
@@ -1,3 +1,28 @@
-import { createTRPCRouter } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
 
-export const boardRouter = createTRPCRouter({});
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { getByIdBoardSchema } from "@/utils/schemas";
+
+export const boardRouter = createTRPCRouter({
+  getById: protectedProcedure
+    .input(getByIdBoardSchema)
+    .query(async ({ ctx, input }) => {
+      const board = await ctx.prisma.board.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!board) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const boardBelongsToUser = board?.userId === ctx.session.user.id;
+
+      if (!boardBelongsToUser) {
+        throw new TRPCError({ code: "FORBIDDEN" });
+      }
+
+      return board;
+    }),
+});
