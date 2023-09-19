b38edf51eaa6241348db5de74a2d2c4c49f31ac4
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-19 01:41:43 -0600

    Refactor dashboard page to use boards - Swap Lists with Boards - Swap CreateList with CreateBoard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { type GetServerSideProps } from "next";
 
-import CreateList from "@/components/CreateList";
-import Lists from "@/components/Lists";
+import Boards from "@/components/Boards";
+import CreateBoard from "@/components/CreateBoard";
 import { getServerAuthSession } from "@/server/auth";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
@@ -21,12 +21,12 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const Dashboard = () => {
   return (
     <main className="min-h-screen pt-16">
-      <Lists />
+      <Boards />
       <div className="flex w-full flex-col items-center justify-center">
         <div className="flex w-full flex-col gap-4 lg:w-1/3">
           <h1 className="text-center text-5xl">Your dashboard</h1>
 
-          <CreateList />
+          <CreateBoard />
         </div>
       </div>
     </main>
