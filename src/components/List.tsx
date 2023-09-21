401c33dd5a9d1633f01459536d4a0445cf58eba9
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-20 21:26:04 -0600

    Add List component

---
+++ src/components/List.tsx
@@ -0,0 +1,29 @@
+import CreateNote from "@/components/CreateNote";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { api, type RouterOutputs } from "@/utils/api";
+
+type ListProps = { list: RouterOutputs["list"]["getById"] };
+
+const List = ({ list }: ListProps) => {
+  const { data: notes } = api.note.getAll.useQuery({ listId: list.id });
+
+  return (
+    <Card className="w-full border lg:w-80">
+      <CardHeader className="p-4 pb-0">
+        <CardTitle className="text-xl">{list.title}</CardTitle>
+      </CardHeader>
+      <CardContent className="p-3">
+        {notes ? (
+          notes.map((note) => {
+            return <p key={note.id}>{note.content}</p>;
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+        <CreateNote listId={list.id} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default List;
