5e463f548bddb2fd9af947e2358b16341af77203
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-24 01:28:31 -0600

    Remove Board date/description

--- src/components/BoardDetails.tsx
+++ src/components/BoardDetails.tsx
@@ -1,13 +1,7 @@
 import { type ComponentPropsWithoutRef } from "react";
 
 import CreateBoard from "@/components/CreateBoard";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { type RouterOutputs } from "@/utils/api";
 
 type BoardDetailsProps = {
@@ -20,14 +14,11 @@ const BoardDetails = ({ board, ...props }: BoardDetailsProps) => {
       className="flex w-full flex-col items-center justify-center"
       {...props}
     >
-      <Card className="w-full lg:w-1/3">
-        <CardHeader>
-          <CardTitle className="text-center text-5xl">
+      <Card>
+        <CardHeader className="px-3 py-2">
+          <CardTitle className="text-center text-2xl">
             {board?.title ?? "Board not found"}
           </CardTitle>
-          <CardDescription className="text-center">
-            {board?.updatedAt.toDateString() ?? "Create one?"}
-          </CardDescription>
         </CardHeader>
         {!board && (
           <CardContent>
