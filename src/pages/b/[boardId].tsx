2f94e41416a27fff5fcf2465d54dd9c806fa12bf
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-25 00:06:49 -0600

    Pad bottom of lists container

--- src/pages/b/[boardId].tsx
+++ src/pages/b/[boardId].tsx
@@ -43,10 +43,11 @@ const BoardPage = () => {
         <BoardDetails board={currentBoard} />
 
         {currentBoard && (
-          <div className="flex h-full items-start gap-2 overflow-x-scroll">
+          <div className="flex h-full items-start gap-2 overflow-x-scroll pb-2">
             {lists?.map((list) => {
               return <List key={list.id} list={list} />;
             })}
+
             <CreateList boardId={id} />
           </div>
         )}
