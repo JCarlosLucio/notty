7211f0f0ffca05034fc5e4d3d5db39a8d2771c31
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-25 00:04:18 -0600

    Make list notes overflow y scrollable - Add CreateNote as CardFooter

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,12 @@
 import CreateNote from "@/components/CreateNote";
 import Note from "@/components/Note";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { api, type RouterOutputs } from "@/utils/api";
 
 type ListProps = { list: RouterOutputs["list"]["getById"] };
@@ -9,11 +15,11 @@ const List = ({ list }: ListProps) => {
   const { data: notes } = api.note.getAll.useQuery({ listId: list.id });
 
   return (
-    <Card className="w-full shrink-0 lg:w-80">
-      <CardHeader className="p-3 pb-0">
+    <Card className="flex max-h-full w-full shrink-0 flex-col lg:w-72">
+      <CardHeader className="shrink-0 p-3 pb-0">
         <CardTitle className="text-md">{list.title}</CardTitle>
       </CardHeader>
-      <CardContent className="p-3">
+      <CardContent className="flex flex-col gap-2 overflow-hidden p-3 hover:overflow-y-scroll ">
         {notes ? (
           notes.map((note) => {
             return <Note key={note.id} note={note} />;
@@ -21,8 +27,10 @@ const List = ({ list }: ListProps) => {
         ) : (
           <p>Loading...</p>
         )}
-        <CreateNote listId={list.id} />
       </CardContent>
+      <CardFooter className="shrink-0 p-3 pt-0">
+        <CreateNote listId={list.id} />
+      </CardFooter>
     </Card>
   );
 };
