30a8ac26779f568dcfe419dba53e04518ce024c9
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-26 15:39:48 -0600

    Setup dnd kit for BoardLists - Add DndContext w/sensors - Add SortableContext to lists

--- src/components/BoardLists.tsx
+++ src/components/BoardLists.tsx
@@ -1,3 +1,11 @@
+import {
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+
 import CreateList from "@/components/CreateList";
 import List from "@/components/List";
 import { api } from "@/utils/api";
@@ -7,14 +15,28 @@ type BoardProps = { boardId: string };
 const BoardLists = ({ boardId }: BoardProps) => {
   const { data: lists } = api.list.getAll.useQuery({ boardId });
 
-  return (
-    <div className="flex h-full items-start gap-2 overflow-x-scroll border border-yellow-500 pb-2">
-      {lists?.map((list) => {
-        return <List key={list.id} list={list} />;
-      })}
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    })
+  );
 
-      <CreateList boardId={boardId} />
-    </div>
+  return (
+    <DndContext sensors={sensors}>
+      <div className="flex h-full items-start gap-2 overflow-x-scroll border border-yellow-500 pb-2">
+        {lists && (
+          <SortableContext items={lists}>
+            {lists.map((list) => {
+              return <List key={list.id} list={list} />;
+            })}
+          </SortableContext>
+        )}
+
+        <CreateList boardId={boardId} />
+      </div>
+    </DndContext>
   );
 };
 
