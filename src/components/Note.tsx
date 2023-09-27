5c09dcc0aa0ea8f3fdafcc85b4c1c495ccc13aa4
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-09-27 12:56:47 -0600

    Make Note sortable

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,11 +1,41 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 import { Button } from "@/components/ui/button";
 import { type RouterOutputs } from "@/utils/api";
 
 type NoteProps = { note: RouterOutputs["note"]["create"] };
 
 const Note = ({ note }: NoteProps) => {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: note.id,
+    data: {
+      type: "Note",
+      note,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Translate.toString(transform),
+  };
+
   return (
-    <Button variant="blur">
+    <Button
+      ref={setNodeRef}
+      variant="blur"
+      className={isDragging ? "border-2 border-destructive" : ""}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
       <span className="w-full text-start">{note.content}</span>
     </Button>
   );
