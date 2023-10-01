8984c41a682f3338830b098fe649024df2ffd7c6
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-10-01 12:04:06 -0600

    Add position when creating notes

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { createNoteSchema, getAllNoteSchema } from "@/utils/schemas";
+import { midString } from "@/utils/sorting";
 
 export const noteRouter = createTRPCRouter({
   getAll: protectedProcedure
@@ -18,10 +19,23 @@ export const noteRouter = createTRPCRouter({
   create: protectedProcedure
     .input(createNoteSchema)
     .mutation(async ({ ctx, input }) => {
+      const notes = await ctx.prisma.note.findMany({
+        where: {
+          listId: input.listId,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      });
+
+      const lastItem = notes.at(-1);
+      const lastPosition = lastItem ? lastItem.position : "";
+
       return await ctx.prisma.note.create({
         data: {
           listId: input.listId,
           content: input.content,
+          position: midString(lastPosition, ""),
         },
       });
     }),
