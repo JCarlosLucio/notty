b4c9211a7988392a3a7eee8af789781bfdf30528
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-10-11 00:21:32 -0600

    Initialize callbackRef with callback

--- src/hooks/useClickAway.ts
+++ src/hooks/useClickAway.ts
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 function useClickAway<T extends HTMLElement>(
   callback: (e: MouseEvent) => void,
 ) {
-  const callbackRef = useRef<typeof callback>(); // initialize mutable ref, which stores callback
+  const callbackRef = useRef(callback); // initialize mutable ref, which stores callback
   const innerRef = useRef<T>(null); // returned to client, who marks "border" element
 
   // update cb on each render, so second useEffect has access to current value
@@ -13,11 +13,7 @@ function useClickAway<T extends HTMLElement>(
 
   useEffect(() => {
     function handleClick(e: MouseEvent) {
-      if (
-        innerRef.current &&
-        callbackRef.current &&
-        !innerRef.current.contains(e.target as Node)
-      ) {
+      if (innerRef.current && !innerRef.current.contains(e.target as Node)) {
         callbackRef.current(e);
       }
     }
