c32e0fd9c87226dc1990c2b9bf733237e2d3ff62
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-10-19 23:09:27 -0600

    Add UpdateBoard component

---
+++ src/components/UpdateBoard.tsx
@@ -0,0 +1,97 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { type ComponentPropsWithoutRef } from "react";
+import { type SubmitHandler, useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { api, type RouterInputs, type RouterOutputs } from "@/utils/api";
+import { updateBoardSchema } from "@/utils/schemas";
+
+type UpdateBoardProps = {
+  board: RouterOutputs["board"]["getById"];
+} & ComponentPropsWithoutRef<"div">;
+type UpdateBoardInput = RouterInputs["board"]["update"];
+
+const UpdateBoard = ({ board }: UpdateBoardProps) => {
+  const form = useForm<UpdateBoardInput>({
+    resolver: zodResolver(updateBoardSchema),
+    defaultValues: {
+      title: board.title,
+      id: "",
+    },
+  });
+  const { toast } = useToast();
+  const ctx = api.useContext();
+
+  const { mutate: updateBoard, isLoading } = api.board.update.useMutation({
+    onSuccess: (updatedBoard) => {
+      ctx.board.getAll.setData(undefined, (oldBoard) => {
+        return oldBoard
+          ? oldBoard.map((b) => (b.id === updatedBoard.id ? updatedBoard : b))
+          : oldBoard;
+      });
+      ctx.board.getById.setData({ id: board.id }, () => {
+        return updatedBoard;
+      });
+      form.reset();
+      toast({
+        description: "Your board was updated.",
+      });
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        description: "Something went wrong.",
+      });
+    },
+  });
+
+  const onSubmit: SubmitHandler<UpdateBoardInput> = (values) => {
+    values.id = board.id;
+    updateBoard(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <div className="flex items-center gap-2">
+                  <Input
+                    placeholder="Your new board title..."
+                    autoFocus
+                    {...field}
+                  />
+                  <Button
+                    type="submit"
+                    disabled={isLoading}
+                    className={isLoading ? "animate-pulse" : ""}
+                  >
+                    Save
+                  </Button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default UpdateBoard;
