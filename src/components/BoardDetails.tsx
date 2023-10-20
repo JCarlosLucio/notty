636cfeb777aad270e39d1a322878c85a6c90f8ec
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-10-19 23:36:58 -0600

    Add Dialog content - Add UpdateBoard - Add pencil icon for editing title

--- src/components/BoardDetails.tsx
+++ src/components/BoardDetails.tsx
@@ -1,10 +1,18 @@
 import { Pencil1Icon } from "@radix-ui/react-icons";
-import { type ComponentPropsWithoutRef } from "react";
+import { type ComponentPropsWithoutRef, useState } from "react";
 
 import CreateBoard from "@/components/CreateBoard";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import UpdateBoard from "@/components/UpdateBoard";
 import { type RouterOutputs } from "@/utils/api";
 
 type BoardDetailsProps = {
@@ -12,6 +20,8 @@ type BoardDetailsProps = {
 } & ComponentPropsWithoutRef<"div">;
 
 const BoardDetails = ({ board, ...props }: BoardDetailsProps) => {
+  const [showUpdateBoard, setShowUpdateBoard] = useState(false);
+
   return (
     <div
       className="flex w-full flex-col items-center justify-center"
@@ -19,18 +29,54 @@ const BoardDetails = ({ board, ...props }: BoardDetailsProps) => {
     >
       <Card>
         <CardHeader className="flex flex-row items-center gap-2 px-3 py-3">
-          <CardTitle className="text-center text-2xl">
-            {board?.title ?? "Board not found"}
-          </CardTitle>
-          {board && (
+          {board ? (
             <Dialog>
               <DialogTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Pencil1Icon />
-                </Button>
+                <CardTitle className="cursor-pointer text-center text-2xl">
+                  {board.title}
+                </CardTitle>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]"></DialogContent>
+              <DialogContent className="sm:max-w-2xl">
+                {showUpdateBoard ? (
+                  <div className="flex flex-row items-end gap-2">
+                    <div className="w-full">
+                      <UpdateBoard
+                        board={board}
+                        cb={() => setShowUpdateBoard(false)}
+                      />
+                    </div>
+                    <Button
+                      variant="destructive"
+                      onClick={() => setShowUpdateBoard(false)}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                ) : (
+                  <DialogHeader>
+                    <div className="flex flex-row gap-2">
+                      <DialogTitle className="text-2xl">
+                        {board.title}
+                      </DialogTitle>
+                      <Button
+                        size="icon"
+                        variant="ghost"
+                        onClick={() => setShowUpdateBoard(true)}
+                      >
+                        <Pencil1Icon />
+                      </Button>
+                    </div>
+                    <DialogDescription>
+                      {board.updatedAt.toDateString()}
+                    </DialogDescription>
+                  </DialogHeader>
+                )}
+              </DialogContent>
             </Dialog>
+          ) : (
+            <CardTitle className="text-center text-2xl">
+              Board not found
+            </CardTitle>
           )}
         </CardHeader>
         {!board && (
