6f49b68396b7cfeef989bdb47f8e8511f7d2ba67
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-10-25 23:05:32 -0600

    Add Pencil icon on hover - Add bg secondary to ListDetails

--- src/components/ListDetails.tsx
+++ src/components/ListDetails.tsx
@@ -22,49 +22,50 @@ const ListDetails = ({ list, ...props }: ListDetailsProps) => {
   const [showUpdateList, setShowUpdateList] = useState(false);
 
   return (
-    <CardHeader
-      className="flex shrink-0 flex-row items-center gap-2 p-4 pb-2"
-      {...props}
-    >
-      <Dialog>
-        <DialogTrigger asChild>
-          <CardTitle className="cursor-pointer text-center text-2xl">
+    <Dialog>
+      <DialogTrigger asChild>
+        <CardHeader
+          className="group flex shrink-0 cursor-pointer flex-row rounded-t-xl bg-secondary/70 p-3"
+          {...props}
+        >
+          <CardTitle className="w-full truncate pl-3 text-center text-2xl">
             {list.title}
           </CardTitle>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-2xl">
-          {showUpdateList ? (
-            <div className="flex flex-row items-end gap-2">
-              <div className="w-full">
-                <UpdateList list={list} cb={() => setShowUpdateList(false)} />
-              </div>
+          <Pencil1Icon className="invisible group-hover:visible" />
+        </CardHeader>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-2xl">
+        {showUpdateList ? (
+          <div className="flex flex-row items-end gap-2">
+            <div className="w-full">
+              <UpdateList list={list} cb={() => setShowUpdateList(false)} />
+            </div>
+            <Button
+              variant="destructive"
+              onClick={() => setShowUpdateList(false)}
+            >
+              Cancel
+            </Button>
+          </div>
+        ) : (
+          <DialogHeader>
+            <div className="flex flex-row gap-2">
+              <DialogTitle className="text-2xl">{list.title}</DialogTitle>
               <Button
-                variant="destructive"
-                onClick={() => setShowUpdateList(false)}
+                size="icon"
+                variant="ghost"
+                onClick={() => setShowUpdateList(true)}
               >
-                Cancel
+                <Pencil1Icon />
               </Button>
             </div>
-          ) : (
-            <DialogHeader>
-              <div className="flex flex-row gap-2">
-                <DialogTitle className="text-2xl">{list.title}</DialogTitle>
-                <Button
-                  size="icon"
-                  variant="ghost"
-                  onClick={() => setShowUpdateList(true)}
-                >
-                  <Pencil1Icon />
-                </Button>
-              </div>
-              <DialogDescription>
-                {list.updatedAt.toDateString()}
-              </DialogDescription>
-            </DialogHeader>
-          )}
-        </DialogContent>
-      </Dialog>
-    </CardHeader>
+            <DialogDescription>
+              {list.updatedAt.toDateString()}
+            </DialogDescription>
+          </DialogHeader>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 };
 
