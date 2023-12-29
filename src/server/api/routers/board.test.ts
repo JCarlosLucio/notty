fc820250b2373ceba4db99cb5305df9e32f6a3c8
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-12-28 21:46:41 -0600

    Add 'should create a new board' test

--- src/server/api/routers/board.test.ts
+++ src/server/api/routers/board.test.ts
@@ -1,10 +1,13 @@
-import { TRPCError } from "@trpc/server";
+import { type inferProcedureInput, TRPCError } from "@trpc/server";
 import { beforeEach, describe, expect, test } from "bun:test";
 
-import { appRouter } from "@/server/api/root";
+import { type AppRouter, appRouter } from "@/server/api/root";
 import { createInnerTRPCContext } from "@/server/api/trpc";
 import { initialBoards, resetDB, testUser } from "@/utils/test";
 
+type BoardInput = inferProcedureInput<AppRouter["board"]["create"]>;
+
+const testBoardInput: BoardInput = { title: "Board Test" };
 const testSession = {
   user: testUser,
   expires: "1",
@@ -32,6 +35,17 @@ describe("Boards", () => {
       expect(async () => {
         await caller.board.getAll();
       }).toThrow(new TRPCError({ code: "UNAUTHORIZED" }));
+    });
+  });
+
+  describe("creating boards", () => {
+    test("should create a board", async () => {
+      const newBoard = await caller.board.create(testBoardInput);
+
+      expect(newBoard).toMatchObject({
+        title: testBoardInput.title,
+        userId: testUser.id,
+      });
     });
   });
 });
