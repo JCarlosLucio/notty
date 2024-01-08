3de7bdc4ff6a7f7d91399ca5e94e92b8f7f48222
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-01-08 00:25:48 -0600

    Add 'should get list by id' test

--- src/server/api/routers/list.test.ts
+++ src/server/api/routers/list.test.ts
@@ -4,6 +4,7 @@ import { beforeEach, describe, expect, test } from "bun:test";
 import {
   caller,
   getBoardsInDB,
+  getListsInDB,
   initialLists,
   resetDB,
   unauthorizedCaller,
@@ -39,6 +40,28 @@ describe("lists", () => {
       expect(async () => {
         await unauthorizedCaller.list.getAll({ boardId: board.id });
       }).toThrow(new TRPCError({ code: "UNAUTHORIZED" }));
+    });
+  });
+
+  describe("getting lists by id", () => {
+    test("should get list by id", async () => {
+      const boards = await getBoardsInDB();
+      const board = boards[0];
+
+      if (!board) {
+        return expect().fail("Couldn't get board in test");
+      }
+
+      const lists = await getListsInDB(board.id);
+      const listToGet = lists[0];
+
+      if (!listToGet) {
+        return expect().fail("Couldn't get list in test");
+      }
+
+      const list = await caller.list.getById({ id: listToGet.id });
+
+      expect(list).toMatchObject(listToGet);
     });
   });
 });
