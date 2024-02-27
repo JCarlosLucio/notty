10a2aa765709e2a68d167d7db942854a7d0849cc
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-02-27 02:40:42 -0600

    Setup mock Auth for Playwright e2e tests - Add globalSetup (Setup cookies, storageState context, add mock user to db) - Add storage-state.json for playwright tests context - Add "type": "module" to package.json to enable use of "import.meta.dirname" - Update playwright config to use globalSetup / storageState

---
+++ e2e/setup/global.ts
@@ -0,0 +1,63 @@
+import { type BrowserContext, chromium } from "@playwright/test";
+import path from "path";
+
+import { db } from "@/server/db";
+
+/**
+ * https://playwright.dev/docs/auth
+ * https://playwright.dev/docs/test-global-setup-teardown
+ * https://github.com/juliusmarminge/t3-complete/blob/main/e2e/setup/global.ts
+ */
+
+type Cookie = Parameters<BrowserContext["addCookies"]>[0][0];
+const testCookie: Cookie = {
+  name: "next-auth.session-token",
+  value: "d52f0c50-b8e3-4326-b48c-4d4a66fdeb64", // some random id
+  domain: "localhost",
+  path: "/",
+  expires: -1, // expired => forces browser to refresh cookie on test run
+  httpOnly: true,
+  secure: false,
+  sameSite: "Lax",
+};
+
+export default async function globalSetup() {
+  const now = new Date();
+
+  await db.user.upsert({
+    where: {
+      email: "octocat@github.com",
+    },
+    create: {
+      name: "Octocat",
+      email: "octocat@github.com",
+      image: "https://github.com/octocat.png",
+      sessions: {
+        create: {
+          // create a session in db that hasn't expired yet, with the same id as the cookie
+          expires: new Date(now.getFullYear(), now.getMonth() + 1, 0),
+          sessionToken: testCookie.value,
+        },
+      },
+      accounts: {
+        // some random mocked discord account
+        create: {
+          type: "oauth",
+          provider: "discord",
+          providerAccountId: "123456789",
+          access_token: "ggg_zZl1pWIvKkf3UDynZ09zLvuyZsm1yC0YoRPt",
+          token_type: "bearer",
+          scope: "email identify",
+        },
+      },
+    },
+    update: {},
+  });
+
+  const storageState = path.resolve(import.meta.dirname, "storage-state.json");
+  const browser = await chromium.launch();
+  const context = await browser.newContext({ storageState });
+  await context.addCookies([testCookie]);
+  await context.storageState({ path: storageState });
+  await browser.close();
+}
