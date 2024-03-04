ddc3352e477541feaf21f5a4225574830aa5ceeb
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-03 22:58:11 -0600

    Add 'toggle-theme-btn' data-testid to Button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -20,6 +20,7 @@ const ThemeToggle = () => {
       variant="ghost"
       size="icon"
       onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
+      data-testid="toggle-theme-btn"
     >
       {resolvedTheme === "light" ? (
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
