3d85d378a9ab85647669e4cb16ff6ed03dc5e3d4
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-05 11:14:28 -0600

    Separate tests into boards / lists / notes

--- e2e/notty.spec.ts
+++ e2e/notty.spec.ts
@@ -27,80 +27,89 @@ test.describe("notty", () => {
       await page.goto("/dashboard");
     });
 
-    test("should have title / heading", async ({ page }) => {
-      await expect(page).toHaveTitle("Dashboard");
-      await expect(page.getByRole("heading", { level: 1 })).toHaveText(
-        "Your dashboard",
-      );
+    test.describe("boards", () => {
+      test("should have title / heading", async ({ page }) => {
+        await expect(page).toHaveTitle("Dashboard");
+        await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+          "Your dashboard",
+        );
+      });
+
+      test("should create board", async ({ page }) => {
+        await page.getByTestId("board-input").fill("testing");
+        await page.getByTestId("create-board-btn").click();
+
+        await expect(page.getByTestId("toast")).toHaveText(
+          "Your board was created.",
+        );
+        await expect(page).toHaveTitle("testing");
+      });
+
+      test("should create board from my boards 'my boards' sheet", async ({
+        page,
+      }) => {
+        const title = "test from sheet";
+        await page.getByTestId("open-boards-btn").click();
+        await page
+          .getByLabel("My Boards")
+          .getByTestId("board-input")
+          .fill(title);
+        await page
+          .getByLabel("My Boards")
+          .getByTestId("create-board-btn")
+          .click();
+
+        await expect(page.getByTestId("toast")).toHaveText(
+          "Your board was created.",
+        );
+        await expect(page).toHaveTitle(title);
+      });
     });
 
-    test("should create board", async ({ page }) => {
-      await page.getByTestId("board-input").fill("testing");
-      await page.getByTestId("create-board-btn").click();
+    test.describe("lists", () => {
+      test("should create list on a board", async ({ page }) => {
+        const title = "test list";
+        await page.getByTestId("open-boards-btn").click();
+        await page.getByTestId("board-link").first().click();
+        await page
+          .getByLabel("My Boards")
+          .getByRole("button", { name: "Close" })
+          .click();
+        await page.getByTestId("show-add-list-btn").click();
+        await page.getByTestId("list-input").fill(title);
+        await page.getByTestId("create-list-btn").click();
 
-      await expect(page.getByTestId("toast")).toHaveText(
-        "Your board was created.",
-      );
-      await expect(page).toHaveTitle("testing");
+        await expect(page.getByTestId("toast")).toHaveText(
+          "Your list was created.",
+        );
+        await expect(page.getByTestId("list")).toHaveCount(2);
+        await expect(page.getByTestId("list").last()).toContainText(title);
+      });
     });
 
-    test("should create board from my boards 'my boards' sheet", async ({
-      page,
-    }) => {
-      const title = "test from sheet";
-      await page.getByTestId("open-boards-btn").click();
-      await page.getByLabel("My Boards").getByTestId("board-input").fill(title);
-      await page
-        .getByLabel("My Boards")
-        .getByTestId("create-board-btn")
-        .click();
+    test.describe("notes", () => {
+      test("should create note on a list", async ({ page }) => {
+        const title = "test note";
+        await page.getByTestId("open-boards-btn").click();
+        await page.getByTestId("board-link").first().click();
+        await page
+          .getByLabel("My Boards")
+          .getByRole("button", { name: "Close" })
+          .click();
+        await page.getByTestId("show-add-note-btn").first().click();
+        await page.getByTestId("note-input").fill(title);
+        await page.getByTestId("create-note-btn").click();
 
-      await expect(page.getByTestId("toast")).toHaveText(
-        "Your board was created.",
-      );
-      await expect(page).toHaveTitle(title);
-    });
-
-    test("should create list on a board", async ({ page }) => {
-      const title = "test list";
-      await page.getByTestId("open-boards-btn").click();
-      await page.getByTestId("board-link").first().click();
-      await page
-        .getByLabel("My Boards")
-        .getByRole("button", { name: "Close" })
-        .click();
-      await page.getByTestId("show-add-list-btn").click();
-      await page.getByTestId("list-input").fill(title);
-      await page.getByTestId("create-list-btn").click();
-
-      await expect(page.getByTestId("toast")).toHaveText(
-        "Your list was created.",
-      );
-      await expect(page.getByTestId("list")).toHaveCount(2);
-      await expect(page.getByTestId("list").last()).toContainText(title);
-    });
-
-    test("should create note on a list", async ({ page }) => {
-      const title = "test note";
-      await page.getByTestId("open-boards-btn").click();
-      await page.getByTestId("board-link").first().click();
-      await page
-        .getByLabel("My Boards")
-        .getByRole("button", { name: "Close" })
-        .click();
-      await page.getByTestId("show-add-note-btn").first().click();
-      await page.getByTestId("note-input").fill(title);
-      await page.getByTestId("create-note-btn").click();
-
-      await expect(page.getByTestId("toast")).toHaveText(
-        "Your note was created.",
-      );
-      await expect(
-        page.getByTestId("list").first().getByTestId("note"),
-      ).toHaveCount(2);
-      await expect(
-        page.getByTestId("list").first().getByTestId("note").last(),
-      ).toContainText(title);
+        await expect(page.getByTestId("toast")).toHaveText(
+          "Your note was created.",
+        );
+        await expect(
+          page.getByTestId("list").first().getByTestId("note"),
+        ).toHaveCount(2);
+        await expect(
+          page.getByTestId("list").first().getByTestId("note").last(),
+        ).toContainText(title);
+      });
     });
   });
 });
