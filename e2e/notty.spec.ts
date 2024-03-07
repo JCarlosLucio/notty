9b61df59d06d8258f78574513d1bc3206314af97
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-07 01:05:26 -0600

    Add 'should move 1st list to 3rd place' test

--- e2e/notty.spec.ts
+++ e2e/notty.spec.ts
@@ -105,6 +105,31 @@ test.describe("notty", () => {
         await expect(page.getByTestId("list").nth(2)).toContainText(/2nd LIST/);
         await expect(page.getByTestId("list").last()).toContainText(/3rd LIST/);
       });
+
+      test("should move 1st list to 3rd place", async ({ page }) => {
+        await page.getByTestId("open-boards-btn").click();
+        await page.getByTestId("board-link").first().click();
+
+        // dragging first list (test list) to 3rd place
+        await page.getByTestId("list").first().hover();
+        await page.mouse.down();
+        await page
+          .getByTestId("list")
+          .getByText(/2nd LIST/)
+          .hover(); // needs double .hover() since dnd implementation uses dragover event
+        await page
+          .getByTestId("list")
+          .getByText(/2nd LIST/)
+          .hover(); // https://playwright.dev/docs/input#drag-and-drop
+        await page.mouse.up();
+
+        await expect(page.getByTestId("list").first()).toContainText(
+          /1st LIST/,
+        );
+        await expect(page.getByTestId("list").nth(1)).toContainText(/2nd LIST/);
+        await expect(page.getByTestId("list").nth(2)).toContainText(title);
+        await expect(page.getByTestId("list").last()).toContainText(/3rd LIST/);
+      });
     });
 
     test.describe("notes", () => {
