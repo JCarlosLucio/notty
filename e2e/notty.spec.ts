5333b82d27afd242949786a6ea01b37f27b52fdc
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-08 01:00:57 -0600

    Add 'should move last note to 2nd list' test

--- e2e/notty.spec.ts
+++ e2e/notty.spec.ts
@@ -304,6 +304,21 @@ test.describe("notty", () => {
         await expect(page.getByTestId("note").nth(3)).toContainText(/4th NOTE/);
         await expect(page.getByTestId("note").last()).toContainText(title);
       });
+
+      test("should move last note to 2nd list", async ({ page }) => {
+        await page.getByTestId("open-boards-btn").click();
+        await page.getByTestId("board-link").first().click();
+
+        // dragging 2nd note (test note) to the last place
+        await page.getByTestId("note").last().hover();
+        await page.mouse.down();
+        await page.getByTestId("list").nth(1).hover(); // needs double .hover() since dnd implementation uses dragover event
+        await page.getByTestId("list").nth(1).hover(); // https://playwright.dev/docs/input#drag-and-drop
+        await page.mouse.up();
+
+        await expect(page.getByTestId("list").first()).not.toContainText(title);
+        await expect(page.getByTestId("list").nth(1)).toContainText(title);
+      });
     });
   });
 });
