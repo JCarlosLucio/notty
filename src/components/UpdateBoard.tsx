a944a890b3ec0f3c3d2d3d64e779df0a8999b3f6
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-25 04:34:46 -0600

    Refactor gradients into gradients utils

--- src/components/UpdateBoard.tsx
+++ src/components/UpdateBoard.tsx
@@ -15,6 +15,7 @@ import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useToast } from "@/components/ui/use-toast";
 import { api, type RouterInputs, type RouterOutputs } from "@/utils/api";
+import { gradients } from "@/utils/gradients";
 import { updateBoardSchema } from "@/utils/schemas";
 
 type UpdateBoardProps = {
@@ -22,79 +23,6 @@ type UpdateBoardProps = {
   cb?: () => void;
 } & ComponentPropsWithoutRef<"div">;
 type UpdateBoardInput = RouterInputs["board"]["update"];
-
-const gradientVariants: { id: string; name: string; bg: string }[] = [
-  {
-    id: "expresso",
-    name: "eXpresso",
-    bg: "linear-gradient(to left, #ad5389, #3c1053)",
-  },
-  {
-    id: "lawrencium",
-    name: "Lawrencium",
-    bg: "linear-gradient(to left, #0f0c29, #302b63, #24243e)",
-  },
-  {
-    id: "argon",
-    name: "Argon",
-    bg: "linear-gradient(to bottom, #03001e, #7303c0, #ec38bc, #fdeff9)",
-  },
-  {
-    id: "earth",
-    name: "Earth",
-    bg: "linear-gradient(to bottom, #00c9ff, #92fe9d)",
-  },
-  {
-    id: "eternal",
-    name: "Eternal",
-    bg: "linear-gradient(to bottom, #09203f 0%, #537895 100%)",
-  },
-  {
-    id: "electric",
-    name: "Electric",
-    bg: "linear-gradient(to top, #4776e6, #8e54e9)",
-  },
-  {
-    id: "love-kiss",
-    name: "Love Kiss",
-    bg: "linear-gradient(to top, #ff0844 0%, #ffb199 100%)",
-  },
-  {
-    id: "twilight",
-    name: "Twilight",
-    bg: "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
-  },
-  {
-    id: "cool",
-    name: "Cool",
-    bg: "linear-gradient(to top, #30cfd0 0%, #330867 100%)",
-  },
-  {
-    id: "cute",
-    name: "Cute",
-    bg: "linear-gradient(to right, #00dbde 0%, #fc00ff 100%)",
-  },
-  {
-    id: "sky",
-    name: "Sky",
-    bg: "linear-gradient(to bottom, #0acffe 0%, #495aff 100%)",
-  },
-  {
-    id: "salt",
-    name: "Salt",
-    bg: "linear-gradient(to top, #FFFEFF 0%, #D7FFFE 100%)",
-  },
-  {
-    id: "magic",
-    name: "Magic",
-    bg: "linear-gradient(-225deg, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%)",
-  },
-  {
-    id: "sunset",
-    name: "Sunset",
-    bg: "linear-gradient(-225deg, #231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%)",
-  },
-];
 
 const UpdateBoard = ({ board, cb }: UpdateBoardProps) => {
   const form = useForm<UpdateBoardInput>({
