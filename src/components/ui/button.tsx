9e0bc3ffb9785a3e9c44026ddaa72da2a133456f
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-03-30 02:36:18 -0600

    Add xl size variant to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,6 +27,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-28 rounded-md",
         icon: "h-9 w-9",
         avatar: "h-9 w-9 rounded-full",
       },
