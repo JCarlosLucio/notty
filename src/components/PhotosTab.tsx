6d05018f915908347e6fd0ef25468473a6b1a17a
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-04-09 01:12:19 -0600

    Add Unsplash disclaimer

--- src/components/PhotosTab.tsx
+++ src/components/PhotosTab.tsx
@@ -16,31 +16,52 @@ const PhotosTab = ({ setBg }: PhotosTabProps) => {
   });
 
   return (
-    <TabsContent
-      value="photos"
-      className="flex overflow-hidden hover:overflow-y-scroll"
-    >
-      <div className="grid w-full grid-cols-3 gap-2">
-        {photos?.map((photo) => (
-          <Button
-            type="button"
-            key={photo.id}
-            size="xl"
-            variant="outline"
-            className="group flex shrink-0 flex-col justify-end"
-            style={{ background: `url(${photo.urls.thumb})` }}
-            onClick={() => setBg(`url(${photo.urls.full})`)}
+    <TabsContent value="photos" className="flex overflow-hidden">
+      <div className="flex flex-col gap-3 pt-2">
+        <div className="flex flex-col overflow-hidden hover:overflow-y-scroll">
+          <div className="grid w-full grid-cols-3 gap-2">
+            {photos?.map((photo) => (
+              <Button
+                type="button"
+                key={photo.id}
+                size="xl"
+                variant="outline"
+                className="group flex shrink-0 flex-col justify-end"
+                style={{ background: `url(${photo.urls.thumb})` }}
+                onClick={() => setBg(`url(${photo.urls.full})`)}
+              >
+                <Link
+                  className="invisible w-full bg-card/50 px-1 text-start group-hover:visible"
+                  href={photo.user.links.html}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {photo.user.name}
+                </Link>
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className="text-xs">
+          By using images from Unsplash, you agree to their{" "}
+          <Link
+            className="hover:underline"
+            href="https://unsplash.com/license"
+            rel="noreferrer"
+            target="_blank"
           >
-            <Link
-              className="invisible w-full bg-card/50 px-1 text-start group-hover:visible"
-              href={photo.user.links.html}
-              rel="noreferrer"
-              target="_blank"
-            >
-              {photo.user.name}
-            </Link>
-          </Button>
-        ))}
+            license
+          </Link>{" "}
+          and{" "}
+          <Link
+            className="hover:underline"
+            href="https://unsplash.com/terms"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Terms of Service
+          </Link>
+        </div>
       </div>
     </TabsContent>
   );
