f8d22c4f9332ab296ee04ba623f20e3940e6e740
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-04-13 17:30:11 -0600

    Change colors to gradients - Update gradients

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -4,17 +4,17 @@ export const gradients: Color[] = [
   {
     id: "expresso",
     name: "eXpresso",
-    bg: "linear-gradient(to left, #ad5389, #3c1053)",
-  },
-  {
-    id: "lawrencium",
-    name: "Lawrencium",
-    bg: "linear-gradient(to left, #0f0c29, #302b63, #24243e)",
+    bg: "linear-gradient(270deg, rgba(0,1,11,1) 0%, rgba(2,3,38,1) 8%, rgba(4,5,64,1) 16%, rgba(6,7,91,1) 24%, rgba(9,9,121,1) 33%, rgba(17,8,121,1) 41%, rgba(27,7,122,1) 50%, rgba(35,6,123,1) 58%, rgba(43,5,123,1) 66%, rgba(33,32,124,1) 74%, rgba(23,59,124,1) 82%, rgba(12,89,125,1) 91%, rgba(1,119,125,1) 100%)",
   },
   {
     id: "argon",
     name: "Argon",
     bg: "linear-gradient(to bottom, #03001e, #7303c0, #ec38bc, #fdeff9)",
+  },
+  {
+    id: "lawrencium",
+    name: "Lawrencium",
+    bg: "linear-gradient(90deg, #020024ff 0%, #090979ff 50%, #7500ffff 100%)",
   },
   {
     id: "earth",
@@ -29,12 +29,12 @@ export const gradients: Color[] = [
   {
     id: "electric",
     name: "Electric",
-    bg: "linear-gradient(to top, #4776e6, #8e54e9)",
+    bg: "linear-gradient(135deg, #4776e6, #8e54e9)",
   },
   {
     id: "love-kiss",
     name: "Love Kiss",
-    bg: "linear-gradient(to top, #ff0844 0%, #ffb199 100%)",
+    bg: "linear-gradient(225deg, #ff0844 0%,#f56b8d 50%,#ffb199 100%)",
   },
   {
     id: "twilight",
@@ -44,7 +44,7 @@ export const gradients: Color[] = [
   {
     id: "cool",
     name: "Cool",
-    bg: "linear-gradient(to top, #30cfd0 0%, #330867 100%)",
+    bg: "linear-gradient(to bottom, #40047cff 0%, #07f29cff 100%)",
   },
   {
     id: "cute",
@@ -54,12 +54,12 @@ export const gradients: Color[] = [
   {
     id: "sky",
     name: "Sky",
-    bg: "linear-gradient(to bottom, #0acffe 0%, #495aff 100%)",
+    bg: "linear-gradient(90deg, rgba(84,51,255,1) 0%, rgba(71,84,255,1) 8%, rgba(57,122,255,1) 17%, rgba(45,156,255,1) 25%, rgba(32,189,255,1) 33%, rgba(69,206,241,1) 42%, rgba(101,222,228,1) 50%, rgba(137,240,214,1) 59%, rgba(165,254,203,1) 66%, rgba(184,254,214,1) 74%, rgba(203,254,225,1) 82%, rgba(225,255,238,1) 91%, rgba(246,255,250,1) 100%)",
   },
   {
     id: "salt",
     name: "Salt",
-    bg: "linear-gradient(to top, #FFFEFF 0%, #D7FFFE 100%)",
+    bg: "linear-gradient(to bottom, #ff00cc 0%, #333399 100%)",
   },
   {
     id: "magic",
@@ -86,57 +86,72 @@ export const gradients: Color[] = [
     name: "Radial",
     bg: "radial-gradient(800px at 700px 200px, rgb(30, 21, 35), rgba(0, 0, 0, 0)), radial-gradient(600px at calc(100% - 300px) 300px, rgb(13, 40, 71), rgba(0, 0, 0, 0)), radial-gradient(800px at 100% 50%, rgb(17, 40, 64), rgba(0, 0, 0, 0)), radial-gradient(800px at 100% 100%, rgb(13, 20, 31), rgba(0, 0, 0, 0)), radial-gradient(800px at calc(50% - 600px) calc(100% - 100px), rgb(55, 23, 47), rgb(25, 17, 23), rgba(0, 0, 0, 0))",
   },
+  {
+    id: "cotton",
+    name: "Cotton",
+    bg: "linear-gradient(90deg, hsla(64, 41%, 92%, 1) 0%, hsla(196, 83%, 84%, 1) 50%, hsla(305, 75%, 83%, 1) 100%)",
+  },
 ];
 
 export const colors: Color[] = [
   {
     id: "night",
     name: "🌃",
-    bg: "#000000",
+    bg: "linear-gradient(to bottom, #000, #000)",
+  },
+  {
+    id: "grey",
+    name: "grey",
+    bg: "linear-gradient(to bottom, #bbd2c5, #536976)",
   },
   {
     id: "white",
     name: "📖",
-    bg: "#ffffff",
+    bg: "linear-gradient(to top, #FFF 0%, #FFF 100%)",
   },
   {
     id: "fish",
     name: "🐟",
-    bg: "#1ec3cf",
+    bg: "linear-gradient(to bottom, #0acffe 0%, #495aff 100%)",
   },
   {
     id: "ocean",
     name: "🩳",
-    bg: "#01096e",
+    bg: "linear-gradient(to bottom, #000428, #004e92)",
   },
   {
     id: "hyacinth",
     name: "🪻",
-    bg: "#5707c0",
+    bg: "linear-gradient(to bottom, #41295a, #2f0743)",
   },
   {
     id: "hibiscus",
     name: "🌺",
-    bg: "#db1fc2",
+    bg: "linear-gradient(to bottom, #ad5389, #7c1880)",
   },
   {
     id: "volcano",
     name: "🌋",
-    bg: "#6b0000",
+    bg: "linear-gradient(to bottom, #870000, #190a05)",
   },
   {
     id: "orange",
     name: "🍊",
-    bg: "#e27900",
+    bg: "linear-gradient(to bottom, #fe8c00, #f83600)",
   },
   {
     id: "chicken",
     name: "🐣",
-    bg: "#dbcf1f",
+    bg: "linear-gradient(to bottom, #ffb347, #ffcc33)",
+  },
+  {
+    id: "avocado",
+    name: "🥑",
+    bg: "linear-gradient(to bottom, #fdfc47, #24fe41)",
   },
   {
     id: "camp",
     name: "🏕️",
-    bg: "#00bb38",
+    bg: "linear-gradient(to bottom, #11998e, #38ef7d)",
   },
 ];
