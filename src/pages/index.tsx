931bf4ec5cd32301e910e3f34aef83418d6c4c0f
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-04-20 17:46:13 -0600

    Add subtitle - Stlye title - Fix spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,16 +28,20 @@ export default function Home() {
         <meta name="description" content="A note taking app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center pt-16">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <span className="bg-gradient-to-r from-emerald-500 to-teal-300 bg-clip-text text-transparent">
-              Notty
-            </span>
-          </h1>
-          <div className="flex flex-col items-center gap-2">
-            <AuthShowcase />
+      <main className="flex min-h-screen flex-col items-center justify-start pt-44">
+        <div className="container flex flex-col items-center justify-center gap-24 pt-20">
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="font-extrabold tracking-tight sm:text-[5rem]">
+              <span className="bg-gradient-to-r from-emerald-500 to-teal-300 bg-clip-text text-9xl text-transparent">
+                Notty
+              </span>
+            </h1>
+            <p className="bg-gradient-to-r from-emerald-500 to-teal-300 bg-clip-text text-lg text-transparent">
+              A simple Kanban board app to help you stay organized anywhere.
+            </p>
           </div>
+
+          <AuthShowcase />
         </div>
       </main>
     </>
