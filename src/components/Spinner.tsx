d3adb02755a8ecdf424a2f72ee4eabe39a5d225a
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-04-26 22:16:31 -0600

    Swap Spinner graphic

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,18 +1,13 @@
 import { cn } from "@/utils/utils";
-import { SVGProps } from "react";
+import { Component1Icon } from "@radix-ui/react-icons";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
 
-const Spinner = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
+const Spinner = ({ className, ...props }: IconProps) => {
   return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
+    <Component1Icon
       className={cn("origin-center animate-spin", className)}
       {...props}
-    >
-      <path d="M2,12A11.2,11.2,0,0,1,13,1.05C12.67,1,12.34,1,12,1a11,11,0,0,0,0,22c.34,0,.67,0,1-.05C6,23,2,17.74,2,12Z" />
-    </svg>
+    />
   );
 };
 
