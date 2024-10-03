29ea3a8edd4269833aca087b4959c6150f753a50
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-10-03 00:01:00 -0600

    Add note skeleton to ListNotes

--- src/components/ListNotes.tsx
+++ src/components/ListNotes.tsx
@@ -1,20 +1,30 @@
 import { SortableContext } from "@dnd-kit/sortable";
 
 import Note from "@/components/Note";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/utils/api";
+import { getRandomArbitrary } from "@/utils/utils";
 
 type ListNotesProps = { listId: string };
 
 const ListNotes = ({ listId }: ListNotesProps) => {
   const { data: notes, isLoading } = api.note.getAll.useQuery({ listId });
 
+  if (isLoading) {
+    return (
+      <>
+        {Array.from({ length: getRandomArbitrary(2, 6) }, (_, index) => (
+          <Skeleton key={index} className="h-12 shrink-0 rounded-md border" />
+        ))}
+      </>
+    );
+  }
+
   return (
     <>
       {notes && (
         <SortableContext items={notes}>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : notes.length > 0 ? (
+          {notes.length > 0 ? (
             notes.map((note) => <Note key={note.id} note={note} />)
           ) : (
             <div className="rounded-xl border border-dashed border-secondary-foreground p-3 opacity-60">
