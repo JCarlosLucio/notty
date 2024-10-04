c56c93a3dfc2eda4cdef900eedf7acfec4c761b2
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-10-03 18:48:28 -0600

    Add BoardDetails to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 
+import BoardDetails from "@/components/BoardDetails";
 import Logo from "@/components/Logo";
 import ThemeToggle from "@/components/ThemeToggle";
 import UserSessionPopover from "@/components/UserSessionPopover";
+import { type RouterOutputs } from "@/utils/api";
 
-const Nav = () => {
+type NavProps = {
+  board?: RouterOutputs["board"]["getById"];
+};
+
+const Nav = ({ board }: NavProps) => {
   return (
     <header className="fixed z-20 h-16 w-full bg-background/60 px-5 backdrop-blur-md md:px-32 2xl:px-60">
       <nav className="flex h-full items-center justify-between">
@@ -17,10 +23,13 @@ const Nav = () => {
             titleId="nav-logo"
             title="notty logo"
           />
-          notty
+          <span className="hidden xl:flex">notty</span>
         </Link>
+
+        {board && <BoardDetails board={board} />}
+
         <div className="flex items-center">
-          <div className="mr-5 flex h-7 items-center border-r pr-5">
+          <div className="mr-5 hidden h-7 items-center border-r pr-5 xl:flex">
             <ThemeToggle />
           </div>
           <UserSessionPopover />
