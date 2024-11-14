397fa8c2c082849c27a4ae0bc7a9eb06e9e7ff3a
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-11-14 01:04:21 -0600

    Increase INFINITE_BOARDS_LIMIT

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const INFINITE_BOARDS_LIMIT = 10;
+export const INFINITE_BOARDS_LIMIT = 16;
 export const MAX_TITLE_LENGTH = 256;
 export const MAX_QUERY_LENGTH = 256;
 export const MAX_CONTENT_LENGTH = 4096;
