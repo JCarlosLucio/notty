eb1e06b0f1e24e007585b67c88c9da805810bc66
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-11-14 00:45:30 -0600

    Increase MAX_CONTENT_LENGTH

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const INFINITE_BOARDS_LIMIT = 10;
 export const MAX_TITLE_LENGTH = 256;
 export const MAX_QUERY_LENGTH = 256;
-export const MAX_CONTENT_LENGTH = 2048;
+export const MAX_CONTENT_LENGTH = 4096;
