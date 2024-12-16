48d63b22da6a90394212fceb9ada924b947c7d20
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-12-16 15:34:29 -0600

    Rearrange CreateBoard/SearchInput in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -53,37 +53,35 @@ const Dashboard = () => {
       <Nav />
       <main className="min-h-screen pb-5 pt-16">
         <div className="flex w-full flex-col items-center gap-8 xl:gap-12">
-          <div className="flex w-full flex-col items-center justify-center">
-            <div className="flex w-full flex-col gap-4 pt-4 lg:w-1/3 xl:pt-0">
-              <h1 className="text-center text-3xl xl:text-5xl">
-                Your dashboard
-              </h1>
-              <div className="px-5">
+          <section className="flex w-5/6 flex-col items-center justify-center gap-8">
+            <h1 className="text-center text-3xl xl:text-5xl">Your dashboard</h1>
+
+            <div className="flex w-full flex-col-reverse items-end justify-between gap-2 lg:flex-row">
+              <div className="w-full lg:w-1/3">
                 <CreateBoard />
               </div>
-
               <SearchInput
                 id="query"
                 type="search"
                 value={query}
                 placeholder="Search boards"
-                className="px-5"
+                className="w-full lg:w-1/3"
                 onChange={(e) => setQuery(e.target.value)}
                 clear={() => setQuery("")}
                 data-testid="search-boards-input"
               />
             </div>
-          </div>
+          </section>
 
           {data?.pages[0]?.boards && data.pages[0].boards?.length === 0 && (
-            <div className="flex flex-col items-center">
+            <section className="flex flex-col items-center">
               <p className="text-xl">No boards yet. 😅</p>
               <p className="text-xl">
                 When you <em>add</em> new boards they will show up here!
               </p>
-            </div>
+            </section>
           )}
-          <div className="grid w-5/6 grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+          <section className="grid w-5/6 grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
             {data?.pages.map((pageData) =>
               pageData.boards.map((board) => (
                 <Button key={board.id} asChild variant="ghost" size="2xl">
@@ -107,7 +105,7 @@ const Dashboard = () => {
               Array.from({ length: INFINITE_BOARDS_LIMIT / 2 }, (_, index) => (
                 <Skeleton key={index} className="h-44 rounded-md" />
               ))}
-          </div>
+          </section>
           {hasNextPage && !isFetching && (
             <Button
               variant="outline"
