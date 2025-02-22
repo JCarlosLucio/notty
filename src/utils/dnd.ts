286e677a8b25531e4e0b694ec7aeafd0d27d5ad2
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-02-21 23:22:29 -0600

    Fix dndkit Error: Maximum update depth exceeded. - Add customCollisionDetectionStrategy - Replaced collisionDetectionStrategy with customCollisionDetectionStrategy - Bug happened when dragging notes over 2 Lists, it detects both as lists and jumps between them until crash. - https://github.com/clauderic/dnd-kit/issues/900#issuecomment-1879870810

--- src/utils/dnd.ts
+++ src/utils/dnd.ts
@@ -1,6 +1,10 @@
 import {
+  closestCenter,
+  closestCorners,
+  type CollisionDetection,
   MouseSensor as LibMouseSensor,
   PointerSensor as LibPointerSensor,
+  pointerWithin,
   TouchSensor as LibTouchSensor,
 } from "@dnd-kit/core";
 import { type MouseEvent, type PointerEvent, type TouchEvent } from "react";
@@ -40,3 +44,21 @@ export class PointerSensor extends LibPointerSensor {
     { eventName: "onPointerDown", handler },
   ] as (typeof LibPointerSensor)["activators"];
 }
+
+// Fixed DNDKIT Error: Maximum update depth exceeded. When dragging notes over/between 2 Lists, it detects both as lists and jumps between them until crash.
+// https://github.com/clauderic/dnd-kit/issues/900
+export const customCollisionDetectionAlgorithm: CollisionDetection = (args) => {
+  const closestCornersCollisions = closestCorners(args);
+  const closestCenterCollisions = closestCenter(args);
+  const pointerWithinCollisions = pointerWithin(args);
+
+  if (
+    closestCornersCollisions.length > 0 &&
+    closestCenterCollisions.length > 0 &&
+    pointerWithinCollisions.length > 0
+  ) {
+    return pointerWithinCollisions;
+  }
+
+  return [];
+};
