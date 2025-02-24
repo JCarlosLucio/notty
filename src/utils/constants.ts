2a3c293105b7115607674917b2c0f30bae171740
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-02-24 16:26:21 -0600

    Add BoardsSkeleton component - Add BOARD_SKELETON_LIMIT constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,5 @@ export const MAX_INFINITE_PHOTOS_PAGES = 3;
 export const MAX_TITLE_LENGTH = 256;
 export const MAX_QUERY_LENGTH = 256;
 export const MAX_CONTENT_LENGTH = 4096;
+
+export const BOARD_SKELETON_LIMIT = 6;
