6a92b01fdde52b49575a71fe2da4fe6ac7793954
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-02-24 13:03:50 -0600

    Hide HexColorInput for smaller screens

--- src/components/UpdateList.tsx
+++ src/components/UpdateList.tsx
@@ -127,7 +127,7 @@ function UpdateList({ list, cb }: UpdateListProps) {
                           onBlur={onBlur}
                         />
                         <HexColorInput
-                          className="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50"
+                          className="border-input placeholder:text-muted-foreground focus-visible:ring-ring hidden h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 md:flex"
                           alpha
                           prefixed
                           color={value ?? undefined}
