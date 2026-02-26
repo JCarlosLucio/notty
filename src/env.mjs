17b148c5c860c51776d4a838d6988eade1fed487
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-02-25 18:30:35 -0600

    Add NEXT_PUBLIC_APP_VERSION environment variable

--- src/env.mjs
+++ src/env.mjs
@@ -45,6 +45,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_APP_VERSION: z.string(),
   },
 
   /**
@@ -61,6 +62,7 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
